ab366af6e881b912ae2dfb8c6bc4fe661ef82f10
BlankaWojcik <blanka1705@wp.pl>
2021-11-29 15:22:01 +0100

    master

---
+++ 03MUST/01arrows.js
@@ -0,0 +1,16 @@
+const getFavouriteNumber = () => 6;
+const addSeven = a => a + 7;
+const divide = (a,b) => a/b;
+
+function getFavouriteNumberA()
+{
+    return 6;
+}
+function addSevenA(a)
+{
+    return a + 7;
+}
+function divideA(a,b) 
+{
+    return a/b;
+}
